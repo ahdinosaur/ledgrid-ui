beeee6d238017ed57b92b9a575210e5064825c52
Michael Williams <dinosaur@riseup.net>
2015-06-21 15:35:33 +1200

    scrolling rainbow

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
+var timers = require('timers')
 var React = require('react')
 var r = require('r-dom')
 var Stylesheet = require('stilr')
+var range = require('lodash.range')
 
 var LedGridUI = require('./')
 
@@ -18,10 +20,23 @@ var stylesheet = document.createElement('style')
 var main = document.createElement('main')
 document.body.appendChild(main)
 
-React.render(
-  r(LedGridUI, {
-    data: ['a', 'b', 'c', 'd'],
-    shape: [2, 2]
-  }),
-  main
-)
+var offset = 0
+timers.setInterval(function () {
+  var height = 32
+  var width = 16
+  var colors = range(height * width).map(function (n) {
+    return {
+      h: (((n / (height * width)) * 360) + offset) % 360,
+      s: 1,
+      v: 1
+    }
+  })
+  React.render(
+    r(LedGridUI, {
+      data: colors,
+      shape: [height, width]
+    }),
+    main
+  )
+  offset += 1
+}, 50)
